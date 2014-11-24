48aa3d9582bc80a00586108afec817bfb5063b68
Wouter1 <W.Pasman@TUDelft.nl>
2014-11-24 11:33:02 +0100

    #3347 EIS must be 0.4 to be GOAL compatible

--- src/main/java/eis/AbstractEISInterface.java
+++ src/main/java/eis/AbstractEISInterface.java
@@ -1,12 +1,10 @@
 package eis;
 
-
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import eis.EIDefaultImpl;
 import eis.exceptions.ActException;
 import eis.exceptions.EntityException;
 import eis.exceptions.NoEnvironmentException;
@@ -19,21 +17,20 @@ public class AbstractEISInterface extends EIDefaultImpl {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	/*
-	 * This map contains references to any object that is
-	 * registered as an entity with EIS.
+	 * This map contains references to any object that is registered as an
+	 * entity with EIS.
 	 */
 	private Map<String, IEISEntity> entities;
-	
+
 	/*
-	 * This map contains references to any action that is
-	 * registered with EIS. The action identifier is used
-	 * to match the action to one of the IEISActions in the
-	 * associated list. 
+	 * This map contains references to any action that is registered with EIS.
+	 * The action identifier is used to match the action to one of the
+	 * IEISActions in the associated list.
 	 */
 	private Map<String, List<IEISAction>> actions;
-	
+
 	public AbstractEISInterface() {
 		entities = new HashMap<String, IEISEntity>();
 		actions = new HashMap<String, List<IEISAction>>();
@@ -43,23 +40,23 @@ public class AbstractEISInterface extends EIDefaultImpl {
 	public <T extends IEISEntity> T getEntity(String name, Class<T> theClass) {
 		return (T) entities.get(name);
 	}
-	
+
 	protected void addEntity(IEISEntity entity) throws EntityException {
 		entities.put(entity.getName(), entity);
 		addEntity(entity.getName(), entity.getType());
 	}
-	
+
 	protected void addAction(IEISAction action) {
 		List<IEISAction> list = actions.get(action.getIdentifier());
 		if (list == null) {
 			list = new LinkedList<IEISAction>();
 			actions.put(action.getIdentifier(), list);
 		}
-		
+
 		action.setInterface(this);
 		list.add(action);
 	}
-	
+
 	/**
 	 * Return the perceptions of the associated entity.
 	 */
@@ -75,24 +72,27 @@ public class AbstractEISInterface extends EIDefaultImpl {
 
 	@Override
 	public String requiredVersion() {
-		return "0.3";
+		return "0.4";
 	}
 
 	private IEISAction getAction(Action action) {
 		List<IEISAction> list = actions.get(action.getName());
-		if (list == null) return null;
-		
-		for (IEISAction act: list) {
+		if (list == null)
+			return null;
+
+		for (IEISAction act : list) {
 			if (act.matches(action)) {
 				return act;
 			}
 		}
 		return null;
 	}
+
 	@Override
 	protected boolean isSupportedByEntity(Action arg0, String arg1) {
 		IEISAction action = getAction(arg0);
-		if (action == null) return false;
+		if (action == null)
+			return false;
 		return action.isEntityAction(arg1);
 	}
 
@@ -105,7 +105,8 @@ public class AbstractEISInterface extends EIDefaultImpl {
 	@Override
 	protected boolean isSupportedByType(Action arg0, String arg1) {
 		IEISAction action = getAction(arg0);
-		if (action == null) return false;
+		if (action == null)
+			return false;
 		return action.isEntityTypeAction(arg1);
 	}
 
@@ -113,8 +114,9 @@ public class AbstractEISInterface extends EIDefaultImpl {
 	protected Percept performEntityAction(String arg0, Action arg1)
 			throws ActException {
 		IEISAction action = getAction(arg1);
-		if (action == null) throw new ActException(ActException.NOTSUPPORTEDBYENTITY);
-		
+		if (action == null)
+			throw new ActException(ActException.NOTSUPPORTEDBYENTITY);
+
 		try {
 			return action.act(arg0, arg1);
 		} catch (ActException ae) { // TODO: check in this small change
